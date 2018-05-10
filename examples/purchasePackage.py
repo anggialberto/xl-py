504beda7db118c89ecaf19bd74a53e75e22f31ca
albertoanggi <albertoanggi@gmail.com>
2018-05-10 21:18:19 +0700

    Update purchasePackage.py

--- examples/purchasePackage.py
+++ examples/purchasePackage.py
@@ -4,7 +4,7 @@ from xlpy import *
 xl = XL('MSISDN/NO.TELP')
 r = xl.loginWithOTP('OTP Code')
 if(r != False):
-    xl.purchasePackage('Service ID')
-    print('Successfully purchased the package')
+    s = xl.purchasePackage('Service ID')
+    print(s['message'])
 else:
     print("Login failed try again")
