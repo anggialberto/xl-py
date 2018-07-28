96efa588ab798d5b89403021d2e5197c44770c64
albertoanggi <albertoanggi@gmail.com>
2018-07-28 19:44:58 +0700

    Update __init__.py

--- xlpy/__init__.py
+++ xlpy/__init__.py
@@ -2,7 +2,7 @@ from .config import Config
 from .client import XL
 
 __copyright__       = 'Copyright 2018 by Anggi Alberto'
-__version__         = '1.0.0'
+__version__         = '3.0.0'
 __author__          = 'Anggi Alberto'
 __author_email__    = 'albertoanggi@gmail.com'
 __url__             = 'http://github.com/albertoanggi/xl-py'
