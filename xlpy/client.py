1a6be456cb60a9c711e5519a27f4da2dc8ec7160
albertoanggi <albertoanggi@gmail.com>
2018-07-28 19:44:20 +0700

    Update client.py

--- xlpy/client.py
+++ xlpy/client.py
@@ -10,94 +10,131 @@ class XL(Config):
         self.msisdn = msisdn
         Config.__init__(self)
     
-    def reqPassword(self):
+    def reqOTP(self):
         payload = {
-            "Body" : {
-                "Header" : {
-                    "ReqID" : self.date,
-                    "IMEI" : self.imei
+            "Header": None,
+            "Body": {
+                "Header": {
+                    "ReqID": self.date,
+                    "IMEI": self.imei
                 },
-                "ForgotPasswordRq" : {
-                    "msisdn" : self.msisdn,
-                    "username" : ""
+                "LoginSendOTPRq": {
+                    "msisdn": self.msisdn
                 }
-            },
-            "sessionId" : None
+            }, 
+            "sessionId": None,
+            "onNet": "False",
+            "platform": "04",
+            "serviceId": "",
+            "packageAmt": "",
+            "reloadType": "",
+            "reloadAmt": "",
+            "packageRegUnreg": "",
+            "appVersion": "3.8.0",
+            "sourceName": "Chrome",
+            "sourceVersion": "",
+            "screenName": "login.enterLoginNumber"
         }
-        r = requests.post(self.XL_HOST_DOMAINV2 + self.XL_PASSRQ_QUERY_PATH, json=payload, headers=self.headers)
+        r = requests.post(self.XL_HOST_DOMAIN + self.XL_OTPRQ_QUERY_PATH, json=payload, headers=self.headers, verify=False)
         status = json.loads(r.content)
         try:
-            if(status['SOAP-ENV:Envelope']['SOAP-ENV:Body'][0]['ns0:CommonResponse'][0]['ns0:ErrorMessage'] == ['SUCCESS_FORGOT_PASSWORDMYXL_NEW']):
-                return {"message" : "Successfully get Password"}
-            else: 
-                return {"message" : "Failed get Password"}
+            if(len(status) == 3):
+                if(status['LoginSendOTPRs']):
+                    return ({'message' : 'Successfully get OTP Code'})
+                else:
+                    return({'message' : status['message']})
+            elif(len(status) == 2):
+                return ({'message' : status['message']})
+            else:
+                return ({'message' : 'Error Parameter'})
         except:
-            return {'message' : status['message']}
-        
-    def loginWithPassword(self, password):
-        params = {"password" : password}
-        r = requests.get("http://xlpy.herokuapp.com/api/encrypt", params=params)
-        status = r.json()
-        payload = {
-            "Body" : {
-                "Header" : {
-                    "IMEI" : self.imei,
-                    "ReqID" : self.date
-                },
-                "LoginV2Rq" : {
-                    "msisdn" : self.msisdn,
-                    "pass" : status["encrypted"]
-                }
-            },
-            "onNet" : "True",
-            "staySigned" : "False",
-            "platform" : "00",
-            "onNetLogin" : "YES",
-            "appVersion" : "3.0.2",
-            "sourceName" : "Android",
-            "sourceVersion" : "5.1"
-        }
-        try:
-            r = requests.post(self.XL_HOST_DOMAINV2 + self.XL_LOGINPWD_QUERY_PATH, json=payload, headers=self.headers)
-        except:
-            r = requests.post(self.XL_HOST_DOMAINV2 + self.XL_LOGINPWD_QUERY_PATH, json=payload, headers=self.headers, verify=False)
-        status = json.loads(r.content)
-        if(len(status) == 6): self._sessionId = status['sessionId']
-        else: return False
+            return ({'message' : 'Uknown Error'})
     
-    def purchasePackage(self, serviceid):        
+    def loginWithOTP(self, otpCode):
         payload = {
-            "Body" : {
-                "HeaderRequest" : {
-                    "applicationID" : "3",
-                    "applicationSubID" : "1",
-                    "touchpoint" : "MYXL",
-                    "requestID" : self.date,
-                    "msisdn" : self.msisdn,
-                    "serviceID" : serviceid
+            "Header": None,
+            "Body": {
+                "Header": {
+                    "ReqID": self.date,
+                    "IMEI": self.imei
                 },
-                "opPurchase" : {
-                    "msisdn" : self.msisdn,
-                    "serviceid" : serviceid
-                },
-                "Header" : {
-                    "IMEI" : self.imei,
-                    "ReqID" : self.date
+                "LoginValidateOTPRq": {
+                    "headerRq": {
+                        "requestDate": self.date[:8],
+                        "requestId": self.date,
+                        "channel": "MYXLPRELOGIN"
+                    },
+                    "msisdn": self.msisdn,
+                    "otp": otpCode
                 }
             },
-            "sessionId" : self._sessionId,
-            "onNet" : "True",
-            "platform" : "00",
-            "staySigned" : "True",
-            "onNetLogin" : "YES",
-            "appVersion" : "3.0.2",
-            "sourceName" : "Android",
-            "sourceVersion" : "5.1"
+            "sessionId": None,
+            "platform": "04",
+            "msisdn_Type": "P",
+            "serviceId": "",
+            "packageAmt": "",
+            "reloadType": "",
+            "reloadAmt": "",
+            "packageRegUnreg": "",
+            "appVersion": "3.8.0",
+            "sourceName": "Chrome",
+            "sourceVersion": "",
+            "screenName": "login.enterLoginOTP",
+            "mbb_category": ""
         }
-        try:
-            r = requests.post(self.XL_HOST_DOMAINV2 + self.XL_PURCHASEPKG_QUERY_PATHV2, json=payload, headers=self.headers)
-        except:
-            r = requests.post(self.XL_HOST_DOMAINV2 + self.XL_PURCHASEPKG_QUERY_PATHV2, json=payload, headers=self.headers, verify=False)
+        r = requests.post(self.XL_HOST_DOMAIN + self.XL_LOGIN_QUERY_PATH , json=payload, headers=self.headers, verify=False)
         status = json.loads(r.content)
-        if(len(status) == 4): return {"message" : "Successfully purchased the package"}
-        else: return {"message" : status['message']}
+        try:
+            if(len(status) == 5):
+                self._sessionId = status['sessionId']
+            else:
+                return False
+        except:
+            pass
+    
+    def purchasePackage(self, serviceid):
+        payload = {
+            "Header": None,
+            "Body": {
+                "HeaderRequest": {
+                    "applicationID": "3",
+                    "applicationSubID": "1",
+                    "touchpoint": "MYXL",
+                    "requestID": self.date,
+                    "msisdn": self.msisdn,
+                    "serviceID": serviceid
+            },
+            "opPurchase": {
+                "msisdn": self.msisdn,
+                "serviceid": serviceid
+            },
+            "XBOXRequest": {
+                "requestName": "GetSubscriberMenuId"
+            },
+            "Header": {
+                "IMEI": self.imei,
+                "ReqID": self.date
+            }
+          },
+          "sessionId": self._sessionId,
+          "serviceId": serviceid,
+          "packageRegUnreg": "Reg",
+          "reloadType": "",
+          "reloadAmt": "",
+          "platform": "04",
+          "appVersion": "3.8.0",
+          "sourceName": "Chrome",
+          "sourceVersion": "",
+          "msisdn_Type": "P",
+          "screenName": "home.storeFrontReviewConfirm",
+          "mbb_category": ""
+        }
+        r = requests.post(self.XL_HOST_DOMAIN + self.XL_PURCHASEPKG_QUERY_PATH , json=payload, headers=self.headers, verify=False)
+        status = json.loads(r.content)
+        try:
+            if(len(status) == 4):
+                return({'message' : 'Successfully purchased package'})
+            else:
+                return({'message' : status['message']})
+        except:
+            return({'message' : 'Uknown Error'})
